179a97ffa36d6e4dd8887b8afcf9d92a0c019cb1
Alessandro D'Alberti <alessandro.dalberti@mail.polimi.it>
2023-11-30 12:08:37 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WHO_Interactive_Checklist"
+include(":app")
+ 
