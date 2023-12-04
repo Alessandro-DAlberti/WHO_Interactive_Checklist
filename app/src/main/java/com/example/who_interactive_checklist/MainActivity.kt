6dde43409c18d3f74d6bc798df9127e009d6d44f
Alessandro D'Alberti <alessandro.dalberti@mail.polimi.it>
2023-12-04 13:51:07 +0100

    String replaced with "Android" - Test

--- app/src/main/java/com/example/who_interactive_checklist/MainActivity.kt
+++ app/src/main/java/com/example/who_interactive_checklist/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Marco ciaao Bottura e Alessandro D'Alberti")
+                    Greeting("Android")
                 }
             }
         }
